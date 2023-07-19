5a462823f330739783ce90fda42e42d2bd855faa
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-19 21:20:32 +0800

    chore: change keymap for jsdoc

--- lua/plugins/1-coding.lua
+++ lua/plugins/1-coding.lua
@@ -124,7 +124,7 @@ return {
     ft = "javascript,typescript,typescriptreact,svelte",
     cmd = "JsDoc",
     keys = {
-      { "<leader>d", "<cmd>JsDoc<cr>", desc = "JsDoc" },
+      { "<leader>jd", "<cmd>JsDoc<cr>", desc = "JsDoc" },
     },
     build = "make install",
   },
