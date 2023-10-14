ee8fd7d3583f6a69d303a9fe5a457f87c7e44b80
Huynh Duc Dung <dunghd.it@gmail.com>
2023-10-14 13:04:39 +0800

    fix: migrate to LazyVim v10

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.ui.alpha" },
+    { import = "lazyvim.plugins.extras.lsp.none-ls" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
