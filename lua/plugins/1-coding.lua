199d447c71fd5c797c7a57abea6a62169872d5d2
Dung Duc Huynh (Kaka) <870029+jellydn@users.noreply.github.com>
2023-12-09 21:07:55 +0800

    feat: add markdown preview

--- lua/plugins/1-coding.lua
+++ lua/plugins/1-coding.lua
@@ -21,4 +21,43 @@ return {
       },
     },
   },
+  -- Markdown
+  {
+    "lukas-reineke/headlines.nvim",
+    opts = function()
+      local opts = {}
+      for _, ft in ipairs({ "markdown", "norg", "rmd", "org" }) do
+        opts[ft] = { headline_highlights = {} }
+        for i = 1, 6 do
+          table.insert(opts[ft].headline_highlights, "Headline" .. i)
+        end
+      end
+      return opts
+    end,
+    ft = { "markdown", "norg", "rmd", "org" },
+    config = function(_, opts)
+      -- PERF: schedule to prevent headlines slowing down opening a file
+      vim.schedule(function()
+        require("headlines").setup(opts)
+        require("headlines").refresh()
+      end)
+    end,
+  },
+  -- Markdown preview
+  {
+    "previm/previm",
+    config = function()
+      -- define global for open markdown preview, let g:previm_open_cmd = 'open -a Safari'
+      vim.g.previm_open_cmd = "open -a Arc"
+    end,
+    ft = { "markdown" },
+    keys = {
+      -- add <leader>mp to open markdown preview
+      {
+        "<leader>mp",
+        "<cmd>PrevimOpen<cr>",
+        desc = "Markdown preview",
+      },
+    },
+  },
 }
