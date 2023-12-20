6ceb03a02630753623004ff7b5b09285aa9e82a4
Huynh Duc Dung <dunghd.it@gmail.com>
2023-12-21 00:41:20 +0800

    feat: add copilot chat
    chore: add keymap
    
    chore: update lock and fix keymap

---
+++ lua/plugins/extras/copilot-chat.lua
@@ -0,0 +1,19 @@
+return {
+  -- Import the copilot plugin
+  { import = "lazyvim.plugins.extras.coding.copilot" },
+  {
+    "jellydn/CopilotChat.nvim",
+    opts = {},
+    build = function()
+      vim.defer_fn(function()
+        vim.cmd("UpdateRemotePlugins")
+        vim.notify("CopilotChat - Updated remote plugins. Please restart Neovim")
+      end, 3000)
+    end,
+    event = "VeryLazy",
+    keys = {
+      { "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
+      { "<leader>cct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
+    },
+  },
+}
