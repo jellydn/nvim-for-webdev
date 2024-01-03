59db5e8b44a2ffff99b12a94c45e6f23b338da90
Huynh Duc Dung <dunghd.it@gmail.com>
2024-01-03 22:14:44 +0800

    chore: use canary branch for latest Copilot Chat

--- lua/plugins/extras/copilot-chat.lua
+++ lua/plugins/extras/copilot-chat.lua
@@ -3,17 +3,18 @@ return {
   { import = "lazyvim.plugins.extras.coding.copilot" },
   {
     "jellydn/CopilotChat.nvim",
+    branch = "canary",
     opts = {},
     build = function()
       vim.defer_fn(function()
         vim.cmd("UpdateRemotePlugins")
-        vim.notify("CopilotChat - Updated remote plugins. Please restart Neovim")
+        vim.notify("CopilotChat - Updated remote plugins. Please restart Neovim.")
       end, 3000)
     end,
     event = "VeryLazy",
     keys = {
       { "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
-      { "<leader>cct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
+      { "<leader>cct", "<cmd>CopilotChatTests<cr>",   desc = "CopilotChat - Generate tests" },
     },
   },
 }
